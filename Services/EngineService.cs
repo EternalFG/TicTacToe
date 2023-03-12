8b7ff30f5e8ace1d4b7ba95135b5f6979eacc3ed
EternalFG <Tesa_L@bk.ru>
2023-03-13 00:42:12 +0200

    Update EngineService

--- Services/EngineService.cs
+++ Services/EngineService.cs
@@ -10,13 +10,9 @@ namespace TicTacToe.Services
 
     public class EngineService : IEngineService
     {
-        public Task<bool> CheckWinner(PlayerSymbol?[,] map) // map[х,y] y - высота
+        public Task<bool> CheckWinner(PlayerSymbol?[,] map)
         {
             // горизонталь
-            //if (CheckStraightHorizontalLine(map, 0, 2).Result || CheckStraightHorizontalLine(map, 1, 2).Result || CheckStraightHorizontalLine(map, 2, 2).Result)
-            //{
-            //    return Task.FromResult(true);
-            //}
 
             if (map[0,0] == PlayerSymbol.O && map[1,0] == PlayerSymbol.O && map[2,0] == PlayerSymbol.O)
             {
@@ -91,37 +87,6 @@ namespace TicTacToe.Services
             return Task.FromResult(false);
         }
 
-        //public Task<bool> CheckStraightHorizontalLine(PlayerSymbol?[,] map, int start, int end)
-        //{
-        //    int X = 0;
-        //    int O = 0;
 
-        //    for (int i = 0; i < map.GetLength(1); i++)
-        //    {
-        //        for (int j = 0; j < map.GetLength(0); j++)
-        //        {
-        //            if (map[start, j] == PlayerSymbol.O)
-        //            {
-        //                O++;
-        //            }
-        //            if (map[start, j] == PlayerSymbol.X)
-        //            {
-        //                X++;
-        //            }
-        //            if (map[start, j] == map[start, end])
-        //            {
-        //                break;
-        //            }
-        //        }
-        //    }
-        //    if (X == 3 || O == 3)
-        //    {
-        //        return Task.FromResult(true);
-        //    }
-        //    else
-        //    {
-        //        return Task.FromResult(false);
-        //    }
-        //}
     }
 }
